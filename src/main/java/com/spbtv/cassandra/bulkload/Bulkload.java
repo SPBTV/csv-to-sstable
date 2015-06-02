194020fb6c1e2bba9a5dba9ca7037291834a9a08
Olivier van den Biggelaar <ovdbigge@spbtv.com>
2015-06-02 18:45:12 +0300

    removing useless imports

--- src/main/java/com/spbtv/cassandra/bulkload/Bulkload.java
+++ src/main/java/com/spbtv/cassandra/bulkload/Bulkload.java
@@ -8,8 +8,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
